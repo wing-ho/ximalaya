ac016fa9fd17d08185ce5c36670db43286959310
Wing <genrong.he@gmail.com>
2024-08-05 17:08:46 +0800

    update decrypt method
    更新加密方法

--- index.js
+++ index.js
@@ -18,23 +18,68 @@ let pageId = 1;
 let quality = 24
 let isAsc = false;
 let albumOrTrack = "album"
+let url;
 
 function getTrackBaseInfo(trackId) {
   let ts = Date.now();
-  return "https://mobile.ximalaya.com/mobile-playpage/track/v3/baseInfo/" + ts + "?device=web&trackId=" + trackId;
+  return "https://www.ximalaya.com/mobile-playpage/track/v3/baseInfo/" + ts + "?device=www2&trackId=" + trackId;
+  // return `https://www.ximalaya.com/mobile-playpage/track/v3/baseInfo/${ts}?device=www2&trackId=${trackId}&trackQualityLevel=2`;
 }
 
 function getAlbumInfo(albumId, pageId) {
   let ts = Date.now();
   // return "https://mobile.ximalaya.com/mobile/v1/album/track/ts-" + ts + "?albumId=" + albumId + "&device=web&isAsc=true&pageId=" + pageId + "&pageSize="+pagesize+"&pre_page=0"
   return "https://mobile.ximalaya.com/mobile/v1/album/track/ts-" + ts + "?albumId=" + albumId + "&device=android&isAsc=" + isAsc + "&isQueryInvitationBrand=true&pageId=" + pageId + "&pageSize=" + pagesize + "&pre_page=0"
+  // return `https://mobile.ximalaya.com/mobile/v1/album/track/ts-${ts}?albumId=${albumId}&device=android&isAsc=${isAsc}&isQueryInvitationBrand=true&pageId=${pageId}&pageSize=${pagesize}&pre_page=0`
 }
+function decryptUrl(encryptedUrl) {  
+  const o = new Uint8Array([183, 174, 108, 16, 131, 159, 250, 5, 239, 110, 193, 202, 153, 137, 251, 176, 119, 150, 47, 204, 97, 237, 1, 71, 177, 42, 88, 218, 166, 82, 87, 94, 14, 195, 69, 127, 215, 240, 225, 197, 238, 142, 123, 44, 219, 50, 190, 29, 181, 186, 169, 98, 139, 185, 152, 13, 141, 76, 6, 157, 200, 132, 182, 49, 20, 116, 136, 43, 155, 194, 101, 231, 162, 242, 151, 213, 53, 60, 26, 134, 211, 56, 28, 223, 107, 161, 199, 15, 229, 61, 96, 41, 66, 158, 254, 21, 165, 253, 103, 89, 3, 168, 40, 246, 81, 95, 58, 31, 172, 78, 99, 45, 148, 187, 222, 124, 55, 203, 235, 64, 68, 149, 180, 35, 113, 207, 118, 111, 91, 38, 247, 214, 7, 212, 209, 189, 241, 18, 115, 173, 25, 236, 121, 249, 75, 57, 216, 10, 175, 112, 234, 164, 70, 206, 198, 255, 140, 230, 12, 32, 83, 46, 245, 0, 62, 227, 72, 191, 156, 138, 248, 114, 220, 90, 84, 170, 128, 19, 24, 122, 146, 80, 39, 37, 8, 34, 22, 11, 93, 130, 63, 154, 244, 160, 144, 79, 23, 133, 92, 54, 102, 210, 65, 67, 27, 196, 201, 106, 143, 52, 74, 100, 217, 179, 48, 233, 126, 117, 184, 226, 85, 171, 167, 86, 2, 147, 17, 135, 228, 252, 105, 30, 192, 129, 178, 120, 36, 145, 51, 163, 77, 205, 73, 4, 188, 125, 232, 33, 243, 109, 224, 104, 208, 221, 59, 9]);
+  const a = new Uint8Array([204, 53, 135, 197, 39, 73, 58, 160, 79, 24, 12, 83, 180, 250, 101, 60, 206, 30, 10, 227, 36, 95, 161, 16, 135, 150, 235, 116, 242, 116, 165, 171]);
 
+  function decodeBase64(input) {
+      return Uint8Array.from(atob(input), c => c.charCodeAt(0));
+  }
+
+  function xorBytes(buffer, offset, key) {
+      const length = Math.min(buffer.length - offset, key.length);
+      for (let i = 0; i < length; i++) {
+          buffer[offset + i] ^= key[i];
+      }
+  }
+
+  try {
+      const decodedUrl = encryptedUrl.replace(/_/g, "/").replace(/-/g, "+");
+      const encryptedData = decodeBase64(decodedUrl);
+      if (encryptedData.length < 16) {
+          return encryptedUrl;
+      }
+
+      const data = encryptedData.slice(0, -16);
+      const iv = encryptedData.slice(-16);
+
+      for (let i = 0; i < data.length; i++) {
+          data[i] = o[data[i]];
+      }
+
+      for (let i = 0; i < data.length; i += 16) {
+          xorBytes(data, i, iv);
+      }
+
+      for (let i = 0; i < data.length; i += 32) {
+          xorBytes(data, i, a);
+      }      
+
+      return new TextDecoder().decode(data);
+  } catch (error) {
+      console.warn(error, "Decryption failed");
+      return "";
+  }
+}
 // 存在只有m4a格式，没有mp3格式的情况
 function getURLFromEncodeDataList(playUrlList, quality) {
   let qualities = ["64", "128", "32", "24"]
   // let types = ["M4A","MP3"]
-  let types = ["MP3", "M4A","AAC"]
+  let types = ["MP3", "M4A", "AAC"]
   let item
   loop:
   for (let i = 0; i < types.length; i++) {
@@ -48,17 +93,20 @@ function getURLFromEncodeDataList(playUrlList, quality) {
       }
     }
   }
-  return CryptoJS.AES.decrypt({ ciphertext: CryptoJS.enc.Base64url.parse(item.url) },
-    CryptoJS.enc.Hex.parse("aaad3e4fd540b0f79dca95606e72bf93"),
-    { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 }).toString(CryptoJS.enc.Utf8);
+  return decryptUrl(item.url)
+  // return CryptoJS.AES.decrypt({ ciphertext: CryptoJS.enc.Base64url.parse(item.url) },
+  //   CryptoJS.enc.Hex.parse("aaad3e4fd540b0f79dca95606e72bf93"),
+  //   { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 }).toString(CryptoJS.enc.Utf8);
 }
 
+
+
 function main() {
-  let url = process.argv[2]
+  url = process.argv[2]
   if (!url) {
-    url = "https://www.ximalaya.com/album/19226647"
-    usage();
-    return;
+    url = "https://www.ximalaya.com/album/4264862"
+    // usage();
+    // return;
   }
   access(dest, (err) => {
     if (err) {
@@ -79,7 +127,7 @@ function main() {
   } else {
     albumId = groups[2]
     url = getAlbumInfo(albumId, pageId)
-    let page = new File(url)
+    let page = new File(url)    
     page.type = "page"
     queue.enqueue(page);
     page.on("downloaded", function () {
@@ -106,12 +154,23 @@ function usage() {
   console.log("Usage: node index.js url dest_folder?")
   console.log("Example: node index.js https://www.ximalaya.com/album/4264862 目录(可选)")
 }
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
 
 class File extends EventEmitter {
   constructor(url) {
     super()
     this.url = url
-    this.filename = path.basename(new URL.URL(url).pathname);
+    this.filename = path.basename(new URL.URL(url).pathname);    
     this.size = 0;
     this.speed = 0;
     this.downloaded = 0
@@ -157,13 +216,19 @@ class File extends EventEmitter {
     }
 
     let options = URL.parse(this.url)
+    
     options = Object.assign(options, {
-      headers: {
+      headers: {        
+        'Referer': this.url,
+        'Accept': "application/json, text/javascript, */*; q=0.01",
+        'Content-Type': "application/json;",
+        'Connection': "keep-alive",        
         // set vip cookie
-        // 'Cookie':"1&_token=186186830&F7BE8C60340N82EB86B994AA1929E774B3BCB8C3971A7EAB0DA438457CDCFCE2697B77728ED767M74B673D8FFBC77F_",
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+        // 'Cookie':"1&_token=395507195&AB994A60240N47B8F9FEFF650563C85C670E05E8A120DBBB4D858643B39208BD494D49F9C92887MA29988DDA1902F0_",
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
       }
     })
+    
     request.get(options, (res) => {
       this.state = "response";
       var chunks = [];
@@ -211,7 +276,7 @@ class File extends EventEmitter {
   }
 
   getTrack() {
-    let resData = JSON.parse(this.content);
+    let resData = JSON.parse(this.content);    
     if ("data" in resData && "list" in resData.data) {
       let tracks = resData.data.list;
       let total = resData.data.totalCount;
@@ -219,31 +284,32 @@ class File extends EventEmitter {
       let pageSize = resData.data.pageSize;
       let padlen = String(total).length;
       for (let i = 0; i < tracks.length; i++) {
-        let track = tracks[i]
+        let track = tracks[i]        
         let index = String((pageId - 1) * pageSize + i + 1).padStart(padlen, '0') + "-"
         // 如果音频的标题自带编号，则取消下一行注释，使用标题自带的编号
         // index = ""
-        if (track.isPaid) {
-          let url = getTrackBaseInfo(track.trackId)
-          let payTrack = new File(url)
-          payTrack.type = "track"
-          payTrack.index = index
-          queue.enqueue(payTrack);
-        } else {
-          let mp3 = new File(track.playUrl64)
-          mp3.type = "mp3"
-          mp3.title = index + track.title;
-          queue.enqueue(mp3);
-        }
+        // if (track.isPaid) {
+        let url = getTrackBaseInfo(track.trackId)
+        console.log("getTrackBaseInfo", url)
+        let payTrack = new File(url)
+        payTrack.type = "track"
+        payTrack.index = index
+        queue.enqueue(payTrack);
+        // } else {
+        //   let mp3 = new File(track.playUrl64)
+        //   mp3.type = "mp3"
+        //   mp3.title = index + track.title;
+        //   queue.enqueue(mp3);
+        // }
       }
     }
   }
 
   getMediaFile() {
-    let resData = JSON.parse(this.content);
+    let resData = JSON.parse(this.content);    
     if ("trackInfo" in resData && "playUrlList" in resData.trackInfo) {
-      let track = resData.trackInfo
-      let url = getURLFromEncodeDataList(track.playUrlList, quality)
+      let track = resData.trackInfo      
+      let url = getURLFromEncodeDataList(track.playUrlList, quality)      
       let mp3 = new File(url);
       mp3.type = "mp3"
       mp3.title = this.index + track.title
@@ -278,7 +344,7 @@ class File extends EventEmitter {
 
 }
 class Queue {
-  MAX_THREADS = 5;
+  MAX_THREADS = 1;
   current_threads = 0
   get idle_threads() {
     return this.MAX_THREADS - this.current_threads;
